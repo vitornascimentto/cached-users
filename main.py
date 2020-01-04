bbcde6b88837c68a4fc1bc6d5fa42c2cad759eb5
vitornascimentto <vitornascimentto0@gmail.com>
2020-01-03 22:23:12 -0300

    utils(request_username)

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from utils import get_username
+from utils import get_username, request_username
 
 if __name__ == "__main__":
 
+    CSV = 'cached.csv'
+    URL = 'https://jsonplaceholder.typicode.com/users'
     USERNAME = get_username()
+
+    response_api = request_username(USERNAME, URL, CSV)
+    print(f'API: {response_api}')
--- utils.py
+++ utils.py
@@ -1,7 +1,39 @@
 import sys
+import requests
+import os
 
 def get_username ():
     if len(sys.argv) != 3 and sys.argv[1] != "-username":
         return 'Error: formato de entrada invalido. (Ex: python3 cached_users.py -username Samantha)'
 
     return sys.argv[2]
+
+def request_username (username, url, csv):
+    params = {'username': username}
+
+    api = requests.get(url, params=params)
+
+    result = api.json()[0]
+
+    if float(result['address']['geo']['lat']) < 0:
+        result['hemisferio'] = 'sul'
+    else:
+        result['hemisferio'] = 'norte'
+
+    if os.path.getsize('cached.csv') == 0:
+        with open('cached.csv', 'w') as csv:
+            csv.write('username,email,website,hemisferio\n')
+
+    if os.path.getsize('cached.csv') > 0:
+        with open('cached.csv', 'r') as csv:
+            
+            csv = csv.readlines()
+
+            csv.append(f"{result['username']},{result['email']},{result['website']},{result['hemisferio']}\n")
+            
+            with open('cached.csv', 'w') as arq:
+                arq.writelines(csv)
+
+    return_result = {'email': result['email'], 'website': result['website'], 'hemisferio': result['hemisferio']}
+
+    return return_result
